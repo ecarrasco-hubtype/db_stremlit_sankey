4a894139dc8820e30d532a0ca389116bfd229d30
Emilio Carrasco <ecarrasco@hubtype.com>
2023-11-23 10:29:20 +0100

    -HT = MA

--- config.py
+++ config.py
@@ -2,23 +2,15 @@
 email_company_dic = {
     'hubtype.com': {
         'Multiasistencia': '98bb4472-1fba-4a2e-8a92-9e2ca0ad4ffc',
-        'Easyjet': '28d3635a-4e22-4f83-8f5b-5ebd77181013',
-        'Segur Caixa Adeslas': '6b9a52d9-57f4-4504-8a9d-ef0f8c2cc683',
+        # 'Easyjet': '28d3635a-4e22-4f83-8f5b-5ebd77181013',
+        # 'Segur Caixa Adeslas': '6b9a52d9-57f4-4504-8a9d-ef0f8c2cc683',
     },
-    # "example.com": {
-    #     'Multiasistencia': '98bb4472-1fba-4a2e-8a92-9e2ca0ad4ffc',
-    #     'Easyjet': '28d3635a-4e22-4f83-8f5b-5ebd77181013',
-    #     'Segur Caixa Adeslas': '6b9a52d9-57f4-4504-8a9d-ef0f8c2cc683',
-    # },
     'multiasistencia.com': {
         'Multiasistencia': '98bb4472-1fba-4a2e-8a92-9e2ca0ad4ffc',
     },
     'easyjet.com': {
         'Easyjet': '28d3635a-4e22-4f83-8f5b-5ebd77181013',
     },
-    # 'gmail.com': {
-    #     'Multiasistencia': '98bb4472-1fba-4a2e-8a92-9e2ca0ad4ffc',
-    # },
     'segurcaixa': {
         'Segur Caixa Adeslas': '6b9a52d9-57f4-4504-8a9d-ef0f8c2cc683',
     },
