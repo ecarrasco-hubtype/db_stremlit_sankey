f51033aa33a5abfe2def784f4ac40740feb6ec50
Emilio Carrasco <ecarrasco@hubtype.com>
2023-12-19 12:15:14 +0100

    -INCLUDE IMPORT TEXTS

--- texts.py
+++ texts.py
@@ -1,13 +1,12 @@
-#Front texts
+# Front texts
 main_title = 'Bot visualization'
-sub_title = 'Understand how users navigate your bot'¡
+sub_title = 'Understand how users navigate your bot'
 session_time = 'The parameter "end session time"  of your bot is: '
 
-#Warning texts
+# Warning texts
 no_data = 'No data available'
 tooltip_include_handoff = 'This helps you filter out paths that ended  without hand off'
 tooltip_include_no_hanoff = 'This helps you filter out paths that ended with a handoff'
 tooltip_max_steps = 'Maximum path length (in number of steps) that you want to show'
 tooltip_min_width = 'Minimum number of users going through the same path. Increasing this helps you focus on paths that are more common. This number is AUTO CALCULATED every time you change bot or starting node.'
 tooltip_starting_node = 'This is the node where the user started the conversation. If you want to see all the possible paths, select the welcome node in your flow.'
-
